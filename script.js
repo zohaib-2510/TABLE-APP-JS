fbcf06ba900006124c7660e8346d54ffb4067886
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-08-23 04:06:50 -0700

    done
    done

---
+++ script.js
@@ -0,0 +1,8 @@
+function myBtnadd(){
+    var inp1 = +prompt('ENTER YOUR FIRST VALUE')
+    var inp2 = +prompt('ENTER YOUR SECOND VALUE')
+for(var i=1; i<=inp2; i++){
+
+document.write(inp1 + ' X ' + i + '=' + inp1*i + '<br>' )
+}
+}
